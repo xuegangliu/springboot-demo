aa03db3cd6fd52c0ebb9958e00893a01266169db
guoliang <guoliang@hrtpayment.com>
2018-12-28 15:52:55 +0800

    添加容错机制

--- springcloud-base/springcloud-consumer-service/src/main/java/com/lxg/consumerservice/web/TestController.java
+++ springcloud-base/springcloud-consumer-service/src/main/java/com/lxg/consumerservice/web/TestController.java
@@ -1,16 +1,35 @@
 package com.lxg.consumerservice.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 public class TestController {
 
     @Autowired
     RestTemplate restTemplate;
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getFrom() {
+        return from;
+    }
 
     @RequestMapping(value = "/test1", method = RequestMethod.GET)
     public String add() {
