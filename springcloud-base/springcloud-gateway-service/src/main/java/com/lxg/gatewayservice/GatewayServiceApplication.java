eeba4b438cb03f78cb627595eca820889162a5fe
guoliang <guoliang@hrtpayment.com>
2018-12-29 15:22:31 +0800

    添加网关服务

---
+++ springcloud-base/springcloud-gateway-service/src/main/java/com/lxg/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,24 @@
+package com.lxg.gatewayservice;
+
+import com.lxg.gatewayservice.filter.AccessFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+}
+
